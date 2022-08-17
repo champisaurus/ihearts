692e886c52b675ecb45df2bfbe41d64c45e1fb1b
champisaurus <107365977+champisaurus@users.noreply.github.com>
2022-08-17 10:15:50 -0400

    Add files via upload

---
+++ MusicApp/MusicApp/responses.swift
@@ -0,0 +1,13 @@
+//
+//  responses.swift
+//  MusicApp
+//
+//  Created by Scholar on 8/16/22.
+//
+
+import Foundation
+class answers {
+    static var nameInput = ""
+    static var mood = ""
+
+}
